18aeed18693d814c02d20e65e16ec481475857ee
Alan Kelvin Lopez <alankelvin1992@gmail.com>
2025-08-15 13:44:38 -0500

    Adds a11y, quality of life improvements

--- src/app/dashboard/@weatherInfo/page.tsx
+++ src/app/dashboard/@weatherInfo/page.tsx
@@ -7,6 +7,13 @@ export default async function WeatherInfoPage() {
     const geo = await getGeo();
     const weatherInfo = await getWeather(geo.latitude, geo.longitude);
 
+    const city = geo.city ? decodeURI(geo.city) : "Your Area";
+    const country = geo.country ?? "";
+    const temperature = Math.round(weatherInfo.temperature);
+    const weatherDescription = weatherCodeText[weatherInfo.condition];
+    const weatherEmoji = weatherCodeEmoji[weatherInfo.condition];
+
+
     if (!geo || !weatherInfo) {
         return (
             <div className="p-4 text-center">
@@ -17,12 +24,15 @@ export default async function WeatherInfoPage() {
 
     return (
         <div className="p-4 flex flex-col items-center justify-center text-center bg-[oklch(27.8%_.033_256.848)] rounded-2xl">
-            <p className="text-sm font-semibold mb-2">{decodeURI(geo.city ?? "")}, {geo.country}</p>
-            <span className="text-[6rem] mb-2">
-                {Math.round(weatherInfo.temperature)}°
+            <h2 className="sr-only">Current Weather</h2>
+            <p className="text-sm font-semibold mb-2">
+                {city}{country ? `, ${geo.country}` : ""}
+            </p>
+            <span className="text-[6rem] mb-2" aria-label={`Temperature: ${temperature} degrees celsius`}>
+                {temperature}°
             </span>
-            <p className="text-gray-500" >
-                 {weatherCodeText[weatherInfo.condition]} {weatherCodeEmoji[weatherInfo.condition]}
+            <p className="text-gray-500" aria-label={weatherDescription} >
+                 {weatherDescription} {weatherEmoji}
             </p>
         </div>
     );
